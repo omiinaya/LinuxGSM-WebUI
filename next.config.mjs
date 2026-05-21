bc6b2c80aad63eebcbd51cd7fa23768619160c83
omiinaya <omiinaya@gmail.com>
2026-05-21 05:50:15 -0400

    chore: fix remaining prettier formatting issues

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
-      config.externals = [...config.externals, 'ssh2'];
+      config.externals = [...config.externals, "ssh2"];
     }
     return config;
   },
